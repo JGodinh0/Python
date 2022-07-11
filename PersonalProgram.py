b5da4195e4f644a4fef051db02bd9ff9aa9de3c0
João V Godinho <profissional.godinho@gmail.com>
2022-07-11 19:19:14 -0300

    Create PersonalProgram.py

---
+++ PersonalProgram.py
@@ -0,0 +1,16 @@
+#Desenvolvi pensando em brincar enqt aprendo :)
+#vamos começar com 4 funções, [Soma, Multiplicação, Subtração e Divisão]
+def Soma():
+    x, y = float(input("\n 1º n° ->        ")), float(input("\n 2º n° ->        "))
+    print("Resultado ==>>    " (x+y))
+def Sub():
+    x, y = float(input("\n 1º n° ->        ")), float(input("\n 2º n° ->        "))
+    print("Resultado ==>>    " (x-y))
+def Mult():
+    x, y = float(input("\n 1º n° ->        ")), float(input("\n 2º n° ->        "))
+    print("Resultado ==>>    " (x*y))
+def Div():
+    x, y = float(input("\n 1º n° ->        ")), float(input("\n 2º n° ->        "))
+    print("Resultado ==>>    " (x/y))
+
+comando_calculadora = int(input("\n Bem vindo à Calculadora! \n Comandos:\n Soma -> Digite 1\n Subtração -> Digite 2\n Multiplicação -> Digite 3\n Divisão -> Digite 4\n Sair -> Digite 0"))
